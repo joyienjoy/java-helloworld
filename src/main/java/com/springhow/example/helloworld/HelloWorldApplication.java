25250242f065579be1ae4950a463a0fc6bba0d08
joyienjoy <92083624+joyienjoy@users.noreply.github.com>
2022-11-02 18:52:27 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "-------------HELLO THIS IS ROUND ONE-------------------------";
+        return "-------------HELLO THIS IS ROUND ONE--------------------------";
     }
 
 }
